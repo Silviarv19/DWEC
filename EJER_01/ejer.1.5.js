fab8a597fe06e57824a98df04f0129ba0e55d58e
Silvia <silviarv19@educastur.es>
2026-09-18 14:27:38 +0200

    ejercicio 1.5

---
+++ EJER_01/ejer.1.5.js
@@ -0,0 +1,44 @@
+//array con los estudiantes
+estudiantes =[
+    alumno1 ={
+        nombre: "Silvia",
+        apellidos: "Ruiz",
+        calificacion: 8,
+        aprobado: true
+    },
+
+    alumno2 ={
+        nombre: "Pablo",
+        apellidos: "Ruiz",
+        calificacion: 9,
+        aprobado: true
+    },
+
+    alumno3 ={
+        nombre: "Sergio",
+        apellidos: "Caramazana",
+        calificacion: 1,
+        aprobado: false
+    }
+]
+
+//utilizar .map() para crear un array y añadir id utilizando index
+
+const estudiantesId= estudiantes.map((estudiantes, index) =>({
+    ...estudiantes, id: index +1
+}))
+
+//utilizar .filter() para obtener los estudiantes con un 5 o más
+
+const estudiantesAprob= estudiantes.filter((estudiantes => estudiantes.calificacion >= 5))
+
+//mostrar un mensaje a los alumnos filtrados
+
+console.log(estudiantesAprob.forEach(estudiantes=> {
+    `¡Felicidades ${estudiantes.nombre}, has aprobado con ${estudiantes.calificacion}`
+}))
+
+//comprobar que la propiedad aprobado es coherente
+array.forEach(element => {
+    
+});
--- EJER_01/ejer1.4.js
+++ EJER_01/ejer1.4.js
@@ -5,3 +5,13 @@ const ciudades=["Madrid","Buenos Aires","Tokio", "Nueva York", "París"]
 ciudades.push("Roma")
 
 //crear array y añadir todas 
+let ciudadesMayusculas=ciudades.map(ciudades => ciudades.toUpperCase())
+
+//array ciudadesFiltradas
+let ciudadesFiltradas=ciudades.filter(ciudad => ciudad.length > 6)
+
+//imprimir los arrays
+
+console.log(ciudades)
+console.log(ciudadesMayusculas)
+console.log(ciudadesFiltradas)
